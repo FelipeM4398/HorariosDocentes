22e868b951acc1b9a7d0333c2c86b85af1ca8a11
miguelmorales2207 <46251138+miguelmorales2207@users.noreply.github.com>
2019-10-22 21:59:55 -0500

    Add files via upload
    vista de perfil - usuarios

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -27,31 +27,20 @@
         <div class="menu">
             <div class="item"><a class="{{Request::is('home') ? 'active' : ''}}" href="{{route('home')}}">Inicio</a></div>
 
-            @if(Auth::user()->hasAnyRole(['Docente']))
-            <div class="item"><a class="{{(Request::is('disponibilidad*') || Request::is('usuarios/periodos*')) ? 'active' : ''}}" href="{{route('disponibilidad.index')}}">Disponibilidad</a></div>
-
-            <div class="item">
-                <a class="{{Request::is('usuarios/asignaturas*') ? 'active' : ''}}" href="{{route('usuarios.asignaturas', Auth::user())}}">Mis asignaturas</a></div>
-            @endif
-
-            @if(Auth::user()->hasAnyRole(['Administrador']))
+            @if(Auth::user()->hasRole('Administrador'))
             <div class="item"><a class="{{Request::is('usuarios*') ? 'active' : ''}}" href="{{route('usuarios.index')}}">Usuarios</a></div>
             @endif
 
-            @if(Auth::user()->hasAnyRole(['Administrador', 'Director']))
+            @if(Auth::user()->hasRole('Docente'))
+            <div class="item"><a class="{{(Request::is('disponibilidad*') || Request::is('usuarios/periodos*')) ? 'active' : ''}}" href="{{route('disponibilidad.index')}}">Disponibilidad</a></div>
+            @endif
+
             <div class="item"><a href="#">Facultades</a></div>
-
-            <div class="item"><a class="{{Request::is('programas*') ? 'active' : ''}}" href="{{route('programas.index')}}">Programas</a></div>
-
+            <div class="item"><a class="{{Request::is('programas') ? 'active' : ''}}" href="{{route('programas.index')}}">Programas</a></div>
             <div class="item"><a href="#">Horarios</a></div>
-            <div class="item"><a href="#">Grupos</a></div>
-
             <div class="item"><a class="{{Request::is('asignaturas*') ? 'active' : ''}}" href="{{route('asignaturas.index')}}">Asignaturas</a></div>
-            @endif
-
-            @if(Auth::user()->hasAnyRole(['Coordinador']))
             <div class="item"><a href="#">Salones</a></div>
-            @endif
+            <div class="item"><a class="{{Request::is('jornadas') ? 'active' : ''}}" href="{{route('jornadas.index')}}">Jornadas</a></div>
         </div>
         <div class="logout">
             <span class="top"></span>
@@ -67,31 +56,15 @@
     </div>
     <div class="content">
         <div class="navbar-horizontal">
-
-            @if(Auth::user()->hasAnyRole(['Administrador']))
-            <div>
-                <div class="item" data-toggle="dropdown"><i class="fas fa-cog"></i></div>
-                <div class="dropdown-menu">
-                    <a class="dropdown-item" href="#">Opciones de administrador</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#"><i class="far fa-calendar"></i> Periodos académicos</a>
-                    <a class="dropdown-item" href="#"><i class="fas fa-university"></i> Sedes</a>
-                </div>
-            </div>
-            @endif
-
             <div class="item"><i class="fas fa-bell"></i><span class="badge badge-notify">9</span></div>
-
-            <div>
-                <div class="item" data-toggle="dropdown"><i class="fas fa-user-circle"></i></div>
-                <div class="dropdown-menu">
-                    <a class="dropdown-item" href="#">{{Auth::user()->nombres}} {{Auth::user()->apellidos}}</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="{{ route('informacion.index') }}"><i class="far fa-address-card"></i> Mi información</a>
-                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
-                        <i class="fas fa-sign-out-alt"></i>
-                        {{ __('Salir') }}</a>
-                </div>
+            <div class="item" data-toggle="dropdown"><i class="fas fa-user-circle"></i></div>
+            <div class="dropdown-menu">
+                <a class="dropdown-item" href="#">{{Auth::user()->nombres}} {{Auth::user()->apellidos}}</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="{{ route('informacion.index') }}"><i class="far fa-address-card"></i> Mi información</a>
+                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                    <i class="fas fa-sign-out-alt"></i>
+                    {{ __('Salir') }}</a>
             </div>
         </div>
         <div class="contenido">
