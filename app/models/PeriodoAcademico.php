c17c259e981f6a5ee7437bb6b61cfb9767f5df16
miguelmorales2207 <46251138+miguelmorales2207@users.noreply.github.com>
2019-10-23 13:42:18 -0500

    Modelo de gestión de periodos
    Modelo de gestión de periodo

--- app/models/PeriodoAcademico.php
+++ app/models/PeriodoAcademico.php
@@ -2,42 +2,16 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class PeriodoAcademico extends Model
 {
     protected $table = 'periodos_academicos';
     public $timestamps = false;
+    protected $fillable = [
+        'año',
+        'periodo'
+    ];
 
-    public function horarioDetalle()
-    {
-        return $this->hasMany('App/HorarioDetalle', 'id_periodo');
-    }
-
-    public function disponibilidadDocente()
-    {
-        return $this->hasMany('App/DisponibilidadDocente', 'id_periodo');
-    }
-
-    public function disponibilidadesDias()
-    {
-        return $this->hasManyThrough('App\DisponibilidadDia', 'App\DisponibilidadDocente', 'id_periodo', 'id_dispo');
-    }
-
-    public function scopeAño($query, $año)
-    {
-        if ($año) {
-            return $query->where('año', 'LIKE', "$año%");
-        }
-    }
-
-    public function scopePeriodo($query, $periodo)
-    {
-        if ($periodo) {
-            if ($periodo == "3") {
-                return $query;
-            }
-            return $query->where('periodo', '=', "$periodo");
-        }
-    }
 }
